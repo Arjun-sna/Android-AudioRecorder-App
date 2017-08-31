d84a195e0d1dba654a9fd3790f0d64c5e9a1e3a0
Arjun <arjun@codebrahma.com>
2017-08-31 19:17:52 +0530

    removed unnecessary files

--- app/src/main/java/in/arjsna/voicerecorder/audiovisualization/DbmHandler.java
+++ app/src/main/java/in/arjsna/voicerecorder/audiovisualization/DbmHandler.java
@@ -1,9 +1,5 @@
 package in.arjsna.voicerecorder.audiovisualization;
 
-import android.content.Context;
-import android.media.MediaPlayer;
-import android.media.audiofx.Visualizer;
-import android.speech.SpeechRecognizer;
 import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import java.util.Timer;
@@ -133,57 +129,4 @@ public abstract class DbmHandler<TData> {
      *                  used for smoothing amplitude values.
      */
     protected abstract void onDataReceivedImpl(TData data, int layersCount, float[] dBmArray, float[] ampsArray);
-
-    public static class Factory {
-
-        /**
-         * Create new visualizer dBm handler.
-         * @param context instance of context
-         * @param audioSessionId audio session id
-         * @return new visualizer dBm handler
-         * @see Visualizer
-         */
-        public static VisualizerDbmHandler newVisualizerHandler(@NonNull Context context, int audioSessionId) {
-            return new VisualizerDbmHandler(context, audioSessionId);
-        }
-
-        /**
-         * Create new visualizer dBm handler and wire with media player. At this point handler will set itself as
-         * {@link MediaPlayer.OnPreparedListener} and {@link MediaPlayer.OnCompletionListener} of media player.
-         * @param context instance of context
-         * @param mediaPlayer instance of media player
-         * @return new visualizer dBm handler
-         * @see Visualizer ,
-         * @see VisualizerDbmHandler#setInnerOnPreparedListener(MediaPlayer.OnPreparedListener)
-         * @see VisualizerDbmHandler#setInnerOnCompletionListener(MediaPlayer.OnCompletionListener)
-         */
-        public static VisualizerDbmHandler newVisualizerHandler(@NonNull Context context, @NonNull
-            MediaPlayer mediaPlayer) {
-            return new VisualizerDbmHandler(context, mediaPlayer);
-        }
-
-        /**
-         * Create new speech recognizer dBm handler. Default dBm values [min, max]:  [-2.12, 10.0].
-         * @param context instance of context
-         * @return new speech recognizer dBm handler
-         * @see SpeechRecognizer
-         */
-        public static SpeechRecognizerDbmHandler newSpeechRecognizerHandler(@NonNull Context context) {
-            return new SpeechRecognizerDbmHandler(context);
-        }
-
-        /**
-         * Create new speech recognizer dBm handler.
-         * @param context instance of context
-         * @param minRmsDbValue minimum dBm value
-         * @param maxRmsDbValue maximum dBm value
-         * @return new speech recognizer dBm handler
-         * @see SpeechRecognizer
-         */
-        public static SpeechRecognizerDbmHandler newSpeechRecognizerDbmHandler(@NonNull
-            Context context, float minRmsDbValue, float maxRmsDbValue) {
-            return new SpeechRecognizerDbmHandler(context, minRmsDbValue, maxRmsDbValue);
-        }
-    }
-
 }
