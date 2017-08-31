3bae80a4e8b9013763cb29434b2966f86d8fe111
Arjun <arjun@codebrahma.com>
2017-08-31 15:30:26 +0530

    rx dependency added

--- app/src/main/java/in/arjsna/voicerecorder/activities/MainActivity.java
+++ app/src/main/java/in/arjsna/voicerecorder/activities/MainActivity.java
@@ -59,16 +59,11 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.menu_main, menu);
     return true;
   }
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    // Handle presses on the action bar items
     switch (item.getItemId()) {
       case R.id.action_settings:
         Intent i = new Intent(this, SettingsActivity.class);
