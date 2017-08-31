54ae846ab00181fc6aa4dd25bd117093db30b92b
Arjun <arjun@codebrahma.com>
2017-08-31 15:24:29 +0530

    tab layout library replaced

--- app/src/main/java/in/arjsna/voicerecorder/activities/MainActivity.java
+++ app/src/main/java/in/arjsna/voicerecorder/activities/MainActivity.java
@@ -2,6 +2,7 @@ package in.arjsna.voicerecorder.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -10,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.astuetz.PagerSlidingTabStrip;
 import in.arjsna.voicerecorder.R;
 import in.arjsna.voicerecorder.fragments.FileViewerFragment;
 import in.arjsna.voicerecorder.fragments.RecordFragment;
@@ -19,23 +19,43 @@ public class MainActivity extends AppCompatActivity {
 
   private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-  private PagerSlidingTabStrip tabs;
+  private TabLayout tabs;
   private ViewPager pager;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    initViews();
+    bindEvents();
+  }
 
+  private void initViews() {
     pager = (ViewPager) findViewById(R.id.pager);
     pager.setAdapter(new MyAdapter(getSupportFragmentManager()));
-    tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
-    tabs.setViewPager(pager);
+    pager.setOffscreenPageLimit(2);
+    tabs = (TabLayout) findViewById(R.id.tabs);
+    tabs.setupWithViewPager(pager);
 
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     toolbar.setPopupTheme(R.style.ThemeOverlay_AppCompat_Light);
-    if (toolbar != null) {
-      setSupportActionBar(toolbar);
-    }
+    setSupportActionBar(toolbar);
+  }
+
+  private void bindEvents() {
+    pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabs));
+    tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+      @Override public void onTabSelected(TabLayout.Tab tab) {
+        pager.setCurrentItem(tab.getPosition());
+      }
+
+      @Override public void onTabUnselected(TabLayout.Tab tab) {
+
+      }
+
+      @Override public void onTabReselected(TabLayout.Tab tab) {
+
+      }
+    });
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
