b2ab3d15cf2e7f34ce3829df95c9582e46ed20ae
Arjun <arjun@codebrahma.com>
2017-09-04 10:39:34 +0530

    removed usused layouts

--- app/src/main/java/in/arjsna/voicerecorder/activities/MainActivity.java
+++ app/src/main/java/in/arjsna/voicerecorder/activities/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
   private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-  private TabLayout tabs;
+  //private TabLayout tabs;
   private ViewPager pager;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
@@ -33,29 +33,29 @@ public class MainActivity extends AppCompatActivity {
     pager = (ViewPager) findViewById(R.id.pager);
     pager.setAdapter(new MyAdapter(getSupportFragmentManager()));
     pager.setOffscreenPageLimit(2);
-    tabs = (TabLayout) findViewById(R.id.tabs);
-    tabs.setupWithViewPager(pager);
+    //tabs = (TabLayout) findViewById(R.id.tabs);
+    //tabs.setupWithViewPager(pager);
 
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    toolbar.setPopupTheme(R.style.ThemeOverlay_AppCompat_Light);
-    setSupportActionBar(toolbar);
+    //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    //toolbar.setPopupTheme(R.style.ThemeOverlay_AppCompat_Light);
+    //setSupportActionBar(toolbar);
   }
 
   private void bindEvents() {
-    pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabs));
-    tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-      @Override public void onTabSelected(TabLayout.Tab tab) {
-        pager.setCurrentItem(tab.getPosition());
-      }
-
-      @Override public void onTabUnselected(TabLayout.Tab tab) {
-
-      }
-
-      @Override public void onTabReselected(TabLayout.Tab tab) {
-
-      }
-    });
+    //pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabs));
+    //tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+    //  @Override public void onTabSelected(TabLayout.Tab tab) {
+    //    pager.setCurrentItem(tab.getPosition());
+    //  }
+    //
+    //  @Override public void onTabUnselected(TabLayout.Tab tab) {
+    //
+    //  }
+    //
+    //  @Override public void onTabReselected(TabLayout.Tab tab) {
+    //
+    //  }
+    //});
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
