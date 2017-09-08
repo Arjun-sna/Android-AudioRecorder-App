7f929ae1def9670d99a7afe5af9da7c59913b44a
Arjun <arjun@codebrahma.com>
2017-09-08 23:01:16 +0530

    setting componenet added

---
+++ app/src/main/java/in/arjsna/audiorecorder/SettingBasic.java
@@ -0,0 +1,72 @@
+package in.arjsna.audiorecorder;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.mikepenz.iconics.view.IconicsImageView;
+import horaapps.org.liz.ThemeHelper;
+import horaapps.org.liz.Themed;
+import in.arjsna.audiorecorder.theme.Themed;
+
+public class SettingBasic extends FrameLayout implements Themed {
+    private final String iconString;
+    @StringRes private final int titleRes;
+    @StringRes private final int captionRes;
+    TextView title;
+    TextView caption;
+
+    public SettingBasic(Context context) {
+        this(context, null);
+    }
+
+    public SettingBasic(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SettingBasic(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        setBackgroundResource(R.drawable.ripple);
+
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        inflater.inflate(R.layout.view_setting_basic, this);
+
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.SettingBasic);
+        iconString = a.getString(R.styleable.SettingBasic_settingIcon);
+        titleRes = a.getResourceId(R.styleable.SettingBasic_settingTitle, 0);
+        captionRes = a.getResourceId(R.styleable.SettingBasic_settingCaption, 0);
+        int minimumApi = a.getInteger(R.styleable.SettingBasic_settingMinApi, 0);
+        a.recycle();
+
+        if (Build.VERSION.SDK_INT < minimumApi) setVisibility(GONE);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        ButterKnife.bind(this);
+
+        icon.setIcon(iconString);
+        title.setText(titleRes);
+        caption.setText(captionRes);
+
+        /*
+        setPadding((int) getResources().getDimension(R.dimen.medium_spacing), 0, (int) getResources().getDimension(R.dimen.medium_spacing), 0);
+        setMinimumHeight((int) getResources().getDimension(R.dimen.listitem_height_twoline));
+        */
+        super.onFinishInflate();
+    }
+
+    @Override
+    public void refreshTheme(ThemeHelper themeHelper) {
+
+    }
+
+}
--- app/src/main/java/in/arjsna/audiorecorder/fragments/SettingsFragment.java
+++ app/src/main/java/in/arjsna/audiorecorder/fragments/SettingsFragment.java
@@ -5,6 +5,7 @@ import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+import com.orhanobut.hawk.Hawk;
 import in.arjsna.audiorecorder.BuildConfig;
 import in.arjsna.audiorecorder.MySharedPreferences;
 import in.arjsna.audiorecorder.R;
@@ -30,6 +31,34 @@ public class SettingsFragment extends PreferenceFragment {
       licensesFragment.show(
           ((SettingsActivity) getActivity()).getSupportFragmentManager(),
           "dialog_licenses");
+      return true;
+    });
+
+    Preference themePref = findPreference(getString(R.string.pref_theme));
+    aboutPref.setOnPreferenceClickListener(preference -> {
+      final int originalColor = getPrimaryColor();
+      new ColorsSetting(SettingsActivity.this).chooseColor(R.string.primary_color, new ColorsSetting.ColorChooser() {
+        @Override
+        public void onColorSelected(int color) {
+          Hawk.put(getString(R.string.preference_primary_color), color);
+          updateTheme();
+          updateUiElements();
+        }
+
+        @Override
+        public void onDialogDismiss() {
+          Hawk.put(getString(R.string.preference_primary_color), originalColor);
+          updateTheme();
+          updateUiElements();
+        }
+
+        @Override
+        public void onColorChanged(int color) {
+          Hawk.put(getString(R.string.preference_primary_color), color);
+          updateTheme();
+          updateUiElements();
+        }
+      }, getPrimaryColor());
       return true;
     });
   }
